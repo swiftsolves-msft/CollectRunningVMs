a8730022454dccabf882f822bdddb0dd68787c35
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2017-10-25 01:57:38 -0400

    Add files via upload

---
+++ ExportNoSQLDB.ps1
@@ -0,0 +1,15 @@
+﻿Login-AzureRmAccount
+
+Add-AzureAccount
+
+$Sub = Get-AzureRmSubscription | Out-GridView -PassThru
+
+Set-AzureRmContext -Subscription $Sub.ID
+
+$saContext = (Get-AzureRmStorageAccount -ResourceGroupName $Store.ResourceGroupName -Name $Store.StorageAccountName).Context
+
+$table = Get-AzureStorageTable -Context $saContext
+
+$Rows = Get-AzureStorageTableRowAll -table $table
+
+$Rows | Export-Csv -Path C:\temp\vmobserve.csv
